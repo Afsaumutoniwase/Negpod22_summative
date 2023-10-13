2e80e1bf4b504757b4de42dfb2719e33d3755647
Afsaumutoniwase <a.umutoniwa@alustudent.com>
2023-10-13 12:37:35 -0700

    first question

--- main.sh
+++ main.sh
@@ -1,83 +1,79 @@
 #!/bin/bash
 
-# Function to create a student record
+students_file="students-list_0923.txt"
+
+# Function to create a new student record
 create_student() {
-    read -p "Enter student email: " email
-    read -p "Enter student age: " age
-    read -p "Enter student ID: " student_id
+    echo "Enter student email:"
+    read email
+    echo "Enter student age:"
+    read age
+    echo "Enter student ID:"
+    read student_id
 
-    # Check if the student ID already exists
-    if grep -q "$student_id" students-list_0923.txt; then
-        echo "Student with ID $student_id already exists."
-    else
-        echo "$email $age $student_id" >> students-list_0923.txt
-        echo "Student record added successfully."
-    fi
+    # Append the student information to the students file
+    echo "$email, $age, $student_id" >> "$students_file"
+    echo "Student record added successfully."
 }
 
-# Function to view all student records
+# Function to view all students
 view_students() {
-    echo "Student Email             Age  ID"
-    echo "------------------------------------"
-    cat students-list_0923.txt | while read line; do
-        echo "$line"
-    done
-    echo "------------------------------------"
-}
-
-# Function to delete a student record by ID
-delete_student() {
-    read -p "Enter student ID to delete: " student_id
-    # Use grep to filter out the student with the given ID
-    grep -v "$student_id" students-list_0923.txt > temp.txt
-    mv temp.txt students-list_0923.txt
-    echo "Student with ID $student_id deleted successfully."
-}
-
-# Function to update a student record by ID
-update_student() {
-    read -p "Enter student ID to update: " student_id
-    if grep -q "$student_id" students-list_0923.txt; then
-        read -p "Enter new student email: " new_email
-        read -p "Enter new student age: " new_age
-
-        # Use sed to update the student record
-        sed -i "/$student_id/c$new_email $new_age $student_id" students-list_0923.txt
-        echo "Student with ID $student_id updated successfully."
+    if [ -e "$students_file" ]; then
+        cat "$students_file"
     else
-        echo "Student with ID $student_id does not exist."
+        echo "No students found."
     fi
 }
 
-# Main menu
-while true; do
-    echo "ALU Student Registration System"
-    echo "1. Create student record"
-    echo "2. View all students"
-    echo "3. Delete student record"
-    echo "4. Update student record"
-    echo "5. Exit"
-    read -p "Enter your choice: " choice
+# Function to delete a student record by student ID
+delete_student() {
+    echo "Enter student ID to delete:"
+    read delete_id
 
+    # Use grep to exclude the line with the specified student ID
+    grep -v "$delete_id" "$students_file" > temp.txt
+    mv temp.txt "$students_file"
+    echo "Student record with ID $delete_id deleted."
+}
+
+# Function to update a student record by student ID
+update_student() {
+    echo "Enter student ID to update:"
+    read update_id
+
+    # Use grep to exclude the line with the specified student ID
+    grep -v "$update_id" "$students_file" > temp.txt
+
+    echo "Enter updated student email:"
+    read email
+    echo "Enter updated student age:"
+    read age
+    echo "Enter updated student ID:"
+    read student_id
+
+    # Append the updated student information to the file
+    echo "$email, $age, $student_id" >> temp.txt
+    mv temp.txt "$students_file"
+    echo "Student record with ID $update_id updated."
+}
+
+# Main menu loop
+while true; do
+    echo "ALU Registration System"
+    echo "1. Create Student Record"
+    echo "2. View All Students"
+    echo "3. Delete Student Record"
+    echo "4. Update Student Record"
+    echo "5. Exit"
+
+    read choice
     case $choice in
-        1)
-            create_student
-            ;;
-        2)
-            view_students
-            ;;
-        3)
-            delete_student
-            ;;
-        4)
-            update_student
-            ;;
-        5)
-            echo "Exiting the application."
-            exit 0
-            ;;
-        *)
-            echo "Invalid choice. Please select a valid option."
-            ;;
+        1) create_student;;
+        2) view_students;;
+        3) delete_student;;
+        4) update_student;;
+        5) exit;;
+        *) echo "Invalid choice. Please try again.";;
     esac
 done
+
